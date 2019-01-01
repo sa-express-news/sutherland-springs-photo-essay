f3cde1dc81c5f57f82adff06a2a07fbffac98901
Luke Whyte <lukeawhyte@gmail.com>
2018-12-31 18:54:55 -0600

    Built essay

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -1,26 +1,47 @@
 // @flow
 
-import React 	from 'react';
-import Animate 	from 'grommet/components/Animate';
+import React, { Component } from 'react';
+import Animate 				from 'grommet/components/Animate';
 
 // scss
 import './Photo.scss';
 
-const setInlineStyles = (property, navBarHeight) => {
-	return {
-		[property]: window.innerHeight - navBarHeight - 3,
-	};
+class Photo extends Component {
+	_img: ?HTMLImageElement;
+
+	componentWillUpdate(prevProps) {
+		if (prevProps.slide.url !== this.props.slide.url) {
+			this.removeOldPhoto();
+		}
+	}
+
+	removeOldPhoto() {
+		this._img.src = '';
+	}
+
+	setInlineStyles(property, navBarHeight) {
+		return {
+			[property]: window.innerHeight - navBarHeight - 3,
+		};
+	}
+
+	render() {
+		const { navBarHeight, slide } = this.props;
+		return (
+			<div className="Photo" style={this.setInlineStyles('height', navBarHeight)}>
+				<div className="vertical-pos">
+					<Animate enter={{'animation': 'fade', 'duration': 1500, 'delay': 0}}>
+						<img 
+							ref={c => (this._img = c)}
+							src={slide.url} 
+							alt={slide.caption} 
+							style={this.setInlineStyles('maxHeight', navBarHeight)}
+						/>
+					</Animate>
+				</div>
+			</div>
+		);
+	}
 }
 
-
-export default ({ navBarHeight, slide }) => {
-	return (
-		<div className="Photo" style={setInlineStyles('height', navBarHeight)}>
-			<div className="vertical-pos">
-				<Animate enter={{'animation': 'fade', 'duration': 1500, 'delay': 0}}>
-					<img src={slide.url} alt={slide.caption} style={setInlineStyles('maxHeight', navBarHeight)}/>
-				</Animate>
-			</div>
-		</div>
-	);
-};
+export default Photo
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// *** REACT W/O REDUX ***
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
@@ -8,20 +6,3 @@ import './index.css';
 import '../node_modules/grommet/scss/vanilla/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// *** REACT W/ REDUX ***
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import store from './store';
-// import App from './components/App/App';
-// import './index.css';
-
-// import '../node_modules/grommet/scss/vanilla/index.scss';
-
-
-// ReactDOM.render(
-//   <Provider store={store}><App /></Provider>,
-//   document.getElementById('root')
-// );
